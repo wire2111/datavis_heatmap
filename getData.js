f54aa029d49414c94b15efbb7f982d15a6983502
Drew Mullaney <drewmullaney@gmail.com>
2023-11-20 02:59:32 -0700

    passing fcc tests

--- getData.js
+++ getData.js
@@ -1,5 +1,6 @@
 export async function getData(URL) {
   return await fetch(URL)
     .then(response => response.json())
+    .then(console.log("retrieved: ", URL))
     .then(data => data);
 }
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { mydata } from "./data.js";
 import { getData } from "./getData.js";
 
 const URL = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json"
-//const DATA = await getData(URL);
-const DATA = mydata;
+const DATA = await getData(URL);
+//const DATA = mydata;
 
 
 //#region global vars  
@@ -24,14 +24,23 @@ const barHeight = 35;
 const P = 70;
 const W = P + (barWidth * dataPointsPerRow) + P;
 const H = P + (barHeight * 12) + P;
-
-
+const legendWidth = 400;
+const legendHeight = 30;
 const xMin = d3.min(DATA.monthlyVariance, data => data.year); // 1753
 const xMax = d3.max(DATA.monthlyVariance, data => data.year); // 2015
 const vMin = d3.min(DATA.monthlyVariance, data => data.variance); // -6.976
 const vMax = d3.max(DATA.monthlyVariance, data => data.variance); // 5.228
 const baseTemp = DATA.baseTemperature; // 8.66
- 
+const minTemp = baseTemp + vMin; // 1.684
+const maxTemp = baseTemp + vMax; // 13.888
+const rangeTemp = maxTemp - minTemp;
+const stepsTemp = rangeTemp / 9 // 1.356
+const TEMP_DOMAIN = [minTemp]
+
+for (let i = 1; i < 10; i++) {
+  TEMP_DOMAIN.push(minTemp + (i * stepsTemp))
+}
+
 //#endregion
 
 //#region scaling and axis stuff  
@@ -43,20 +52,6 @@ const xScale = d3.scaleLinear()
 const yScale = d3.scaleTime()
   .domain([new Date(2023, 0), new Date(2023, 11)])
   .range([P + (barHeight / 2), (H - P) - (barHeight / 2)]);
-  
-const vScale = d3.scaleLinear()
-  .domain([vMin, -6, -4, -2, 0, 1, 2, 4, vMax])
-  .range([
-    "rgb(69, 117, 180)",
-    "rgb(116, 173, 209)",
-    "rgb(171, 217, 233)",
-    "rgb(224, 243, 248)",
-    "rgb(255, 255, 191)",
-    "rgb(254, 224, 144)",
-    "rgb(253, 174, 97)",
-    "rgb(244, 109, 67)",
-    "rgb(215, 48, 39)",
-  ]);
 
 const xAxis = d3.axisBottom(xScale).ticks(20, "d");
 const yAxis = d3.axisLeft(yScale).ticks(12, "%B")
@@ -72,20 +67,39 @@ function yStart(d) {
   return y
 }
 
+const legendX = d3.scaleLinear()
+  .domain([minTemp, maxTemp])
+  .range([0, legendWidth])
+
+const vScale = d3.scaleThreshold()
+  .domain(TEMP_DOMAIN)
+  .range([
+    "#4575b4",
+    "#74add1",
+    "#abd9e9",
+    "#e0f3f8",
+    "#ffffbf",
+    "#fee090",
+    "#fdae61",
+    "#f46d43",
+    "#d73027",
+  ]);
+
+const legendXAxis = d3.axisBottom()
+  .scale(legendX)
+  .tickValues(vScale.domain())
+  .tickFormat(d3.format(".1f"))
 
 //#endregion
 
 //#region render stuff  
 
-var tooltip = d3.select("#vis")
+const tooltip = d3.select("#vis")
   .append("div")
     .style("position", "absolute")
     .style("visibility", "hidden")
     .attr("id", "tooltip")
     .text("");
-
-
-
 
 const svg = 
 
@@ -105,6 +119,24 @@ svg.append("g")
   .attr("transform", `translate(${P}, 0)`)
   .call(yAxis);
 
+const legend = svg
+  .append("g")
+  .attr("transform", "translate(20, 40)")
+  .attr("id", "legend")
+  .call(legendXAxis)
+
+legend
+  .append("g")
+  .selectAll("rect")
+  .data(vScale.range())
+  .enter()
+  .append("rect")
+  .style("fill", d => d)
+  .attr("x", (d, i) => {return legendWidth / 9 * i})
+  .attr("y", -legendHeight)
+  .attr("width", legendWidth / 9)
+  .attr("height", legendHeight)
+
 svg.selectAll("rect")
   .data(DATA.monthlyVariance)
   .enter()
@@ -114,7 +146,7 @@ svg.selectAll("rect")
     .attr("y", d => yStart(d))
     .attr("width", barWidth)
     .attr("height", barHeight)
-    .attr("fill", d => vScale(d.variance))
+    .attr("fill", d => vScale(d.variance + baseTemp))
     .attr("data-month", d => d.month - 1) // needs to be 0-11 to pass fcc tests
     .attr("data-year", d => d.year)
     .attr("data-temp", d => baseTemp + d.variance)
@@ -126,7 +158,6 @@ svg.selectAll("rect")
         <p>Temp: ${e.target.dataset.temp}</p>
         <p>Variance: ${e.target.dataset.variance}</p>
       `)
-      console.log(e)
       tooltip.style("top", e.pageY - 100 + "px")
       tooltip.style("left", e.pageX - 30 + "px")
       tooltip.style("visibility", "visible")
